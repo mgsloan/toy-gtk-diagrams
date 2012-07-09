df65725557c4c921b1491b1cfc618e89c55f2109
mgsloan <mgsloan@gmail.com>
2012-07-09 00:02:09 -0700

    rename Toys to examples

--- examples/Animator.hs
+++ examples/Animator.hs
@@ -3,24 +3,32 @@
 module Animator where
 
 import Graphics.UI.Toy.Prelude
+
 import Data.Colour.SRGB (sRGB)
 import Data.Default
+import Data.Time ( UTCTime, UTCTime, getCurrentTime, diffUTCTime )
 
 data Animator = Animator
   { animTime    :: Double
+  , animStart   :: Maybe UTCTime
   , animDiagram :: Double -> CairoDiagram
   }
 
 type instance V Animator = R2
 
 instance Default Animator where
-  def = Animator 0 (const mempty)
+  def = Animator 0 Nothing (const mempty)
 
 instance Interactive Gtk Animator where
-  tick = simpleTick (\(Animator t f) -> Animator (t + 0.03) f)
+  tick _ (Animator _ Nothing d) = do
+    s <- getCurrentTime
+    return $ (Animator 0 (Just s) d, True)
+  tick _ (Animator _ (Just s) d) = do
+    t' <- debug . realToFrac . (`diffUTCTime` s) <$> getCurrentTime
+    return (Animator t' (Just s) d, True)
 
 instance GtkDisplay Animator where
   display = displayDiagram diagram
 
 instance Diagrammable Cairo Animator where
-  diagram (Animator t f) = f t
+  diagram (Animator t _ f) = f t
