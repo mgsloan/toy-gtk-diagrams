96e8850ca2be94839ecd01d2b578c4ad70e7bc13
mgsloan <mgsloan@gmail.com>
2012-07-09 06:42:14 -0700

    Better names

--- examples/KeyMove.hs
+++ examples/KeyMove.hs
@@ -4,25 +4,25 @@ import Graphics.UI.Toy.Prelude
 
 data Sprite = Sprite {
   pos, vel :: R2
-} deriving Show
+}
 
 type instance V Sprite = R2
 
 instance Interactive Gtk Sprite where
-  tick inp s = return (Sprite pos' vel', True)
+  tick input state = return (Sprite pos' vel', True)
    where
-    pos' = pos s       + vel' 
-    vel' = vel s * 0.9 + accel
+    pos' = pos state       + vel' 
+    vel' = vel state * 0.9 + acc
 
-    handleKey c v = r2 $ if keyHeld c inp then v else (0, 0)
+    handleKey key v = r2 $ if keyHeld key input then v else (0, 0)
 
-    accel = handleKey "w" ( 0, -1)
-          + handleKey "a" (-1,  0)
-          + handleKey "s" ( 0,  1)
-          + handleKey "d" ( 1,  0)
+    acc = handleKey "w" ( 0, -1)
+        + handleKey "a" (-1,  0)
+        + handleKey "s" ( 0,  1)
+        + handleKey "d" ( 1,  0)
 
 instance Diagrammable Cairo Sprite where
-  diagram s = translate (pos s) $ circle 20
+  diagram state = translate (pos state) $ circle 20
 
 instance GtkDisplay Sprite where
   display = displayDiagram diagram
