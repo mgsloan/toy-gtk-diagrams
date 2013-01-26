ee18d8111d52d8e77fc229d19cd9635cfebe1a9b
Michael Sloan <mgsloan@gmail.com>
2013-01-26 00:30:55 -0800

    Fancier button diagram / states

--- src/Graphics/UI/Toy/Button.hs
+++ src/Graphics/UI/Toy/Button.hs
@@ -22,10 +22,10 @@
 --
 -----------------------------------------------------------------------------
 module Graphics.UI.Toy.Button
-  ( Button(..), CairoButton
+  ( ButtonState(..), Button(..), CairoButton
  
   -- * Lenses
-  , buttonHeld, buttonHit, buttonDiagram
+  , buttonState, buttonHit, buttonDiagram
 
   -- * Mutation
   , clearButtonHit
@@ -36,6 +36,8 @@ module Graphics.UI.Toy.Button
 
 import Control.Newtype (Newtype, pack)
 import Data.AffineSpace.Point (Point(P))
+import Data.Colour
+import Data.Colour.Names
 import Data.Label
 import Diagrams.Backend.Cairo (Cairo)
 import Diagrams.Backend.Cairo.Text (textLineBounded)
@@ -46,6 +48,12 @@ import Graphics.UI.Toy.Diagrams
 
 type CairoButton = Button Cairo R2
 
+data ButtonState
+  = NormalState
+  | HoverState
+  | PressState
+  deriving (Eq, Ord, Enum, Bounded, Read, Show)
+
 -- | A button stores the state necessary to know if the button is currently
 --   being pressed ('_buttonHeld'), and if it was hit ('_buttonHit').  The
 --   semantics of '_buttonHit' are up to the user, as it's up to the user to
@@ -55,9 +63,10 @@ type CairoButton = Button Cairo R2
 --   it, the function '_buttonDiagram' is used. It draws the button based on
 --   the current '_buttonHeld' state.
 data Button b v = Button
-  { _buttonHeld :: Bool                   -- ^ Whether the mouse is currently pressing
-  , _buttonHit  :: Bool                   -- ^ Whether the button was hit
-  , _buttonDiagram :: Bool -> Diagram b v -- ^ Draw button based on '_buttonHeld' 
+  { _buttonState   :: ButtonState -- ^ Whether the mouse is hovering / pressing.
+  , _buttonHit     :: Bool        -- ^ Whether the button was hit.
+  , _buttonDiagram :: Button b v -> Diagram b v 
+                                  -- ^ Draw button based on the state.
   }
 
 type instance V (Button b v) = v
@@ -66,49 +75,50 @@ $(mkLabels [''Button])
 
 -- | Builds a button, given the function used to draw it.  The first argument
 --   of this function is a boolean that indicates whether it's held.
-mkButton :: (Bool -> Diagram b v) -> Button b v
-mkButton = Button False False
+mkButton :: (Button b v -> Diagram b v) -> Button b v
+mkButton = Button NormalState False
 
 -- | Builds a button containing text.  The outside border is a rounded
---   rectangle, and when pressed, it is drawn with a black fill and white lines.
+--   rectangle, and when pressed, it's drawn with a black fill and white lines.
 mkDefaultButton :: String -> CairoButton
-mkDefaultButton txt = mkButton f
+mkDefaultButton txt = mkButton dia
   where
-    f True = fc black . lc white $ lw 2 dia
-    f False = blackLined dia
-    dia = centerXY label <> centerXY (roundedRect (width label + 5) (height label + 5) 3)
-    label = pad 1 . reflectY $ textLineBounded monoStyle txt
+    dia b = addTint $ case _buttonState b of
+        NormalState -> blackLined $ label <> border
+        HoverState  -> blackLined $ label <> fc lightgray border
+        PressState  -> fc white label <> (border # fc black # lc white)
+      where
+        addTint
+          | _buttonHit b = flip overlayScaled (square 1 # fcA redTint)
+          | otherwise    = id
+        redTint = red `withOpacity` 0.5
+    border = centerXY . lw 2 $ roundedRect (width label + 5) (height label + 5) 3
+    label = centerXY . pad 1 . reflectY $ textLineBounded monoStyle txt
 
--- | This function literaly just 'set's 'buttonHit' to 'False'.
+-- | This function literally just 'set's 'buttonHit' to 'False'.
 clearButtonHit :: Button b v -> Button b v
 clearButtonHit = set buttonHit False
 
 instance ( Newtype v (MousePos ib)
          , HasLinearMap v, InnerSpace v, OrderedField (Scalar v) )
       => Interactive ib (Button b v) where
-  mouse (Just (c, 0)) i b
-    | ci && c   = return
-                . set buttonHeld True
-                $ b
-    | not   c   = return
-                . set buttonHeld False 
-                . set buttonHit  (ci && get buttonHeld b)
-                $ b
-    | otherwise = return b
+  mouse m i b = return $ transition b
    where
     ci = clickInside b . P . pack $ mousePos i
-    
-  mouse _ _ b = return b
+    transition = case (ci, m, _buttonState b) of
+      (True, Just (True,  0),          _) -> set buttonState PressState
+      (True, Just (False, 0), PressState) -> set buttonState HoverState . set buttonHit True
+      (True,               _, PressState) -> id
+      (True,               _,          _) -> set buttonState HoverState
+      (False,              _,          _) -> set buttonState NormalState
 
 instance Diagrammable b v (Button b v) where
-  diagram x = get buttonDiagram x $ get buttonHeld x
+  diagram x = get buttonDiagram x x
 
-instance ( InnerSpace v, HasLinearMap v, OrderedField (Scalar v)
-         ) => Enveloped (Button b v)
- where
+instance (InnerSpace v, HasLinearMap v, OrderedField (Scalar v))
+      => Enveloped (Button b v) where
   getEnvelope b = getEnvelope (diagram b :: Diagram b v)
 
-instance ( InnerSpace v, HasLinearMap v, OrderedField (Scalar v)
-         ) => Clickable (Button b v)
- where
+instance (InnerSpace v, HasLinearMap v, OrderedField (Scalar v))
+      => Clickable (Button b v) where
   clickInside b = clickInside (diagram b :: Diagram b v)
